7d0759e7ebfdc6bba7f63b60a1ea221159b1d3db
olivierfradette-roy <olivier.fradette.roy@gmail.com>
2020-09-07 20:57:13 -0400

    display resume picture

--- src/cv.vue
+++ src/cv.vue
@@ -1,53 +1,15 @@
 <template>
-    <div>
-        <h1>Olivier Fradette-Roy</h1>
-        <ul class="breadcrumb">
-            <li v-for="item in items" :key="item.href">
-                <a v-show="item.href && item.href.length>0 && !item.active" :href="item.href">{{item.text}}</a>
-                <span v-show="item.active && item.active === true">{{item.text}}</span>
-            </li>
-        </ul>
+    <div style="width: 100%">
+        <v-row justify="center">
+            <img src="./assets/resume.png"/>
+        </v-row>
     </div>
 </template>
 
 <script>
     export default {
         name: 'cv',
-        props: {
-            items: {
-                type: Array,
-                required: true,
-                default: () => []
-            }
-        }
     };
 </script>
 
-<style scoped>
-    ul.breadcrumb {
-        padding: 10px 16px;
-        list-style: none;
-        background-color: #eee;
-    }
-
-    ul.breadcrumb li {
-        display: inline;
-        font-size: 18px;
-    }
-
-    ul.breadcrumb li + li:before {
-        padding: 8px;
-        color: black;
-        content: "/\00a0";
-    }
-
-    ul.breadcrumb li a {
-        color: #0275d8;
-        text-decoration: none;
-    }
-
-    ul.breadcrumb li a:hover {
-        color: #01447e;
-        text-decoration: underline;
-    }
-</style>
+<style scoped></style>
